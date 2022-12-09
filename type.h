f8de84ac100b2b1a8b3f5768bebf11cb772e891a
Keyno <100038382+KeynoIX@users.noreply.github.com>
2022-12-09 09:54:53 +0000

    sauvegarde du 9/12/2022 10:54

--- type.h
+++ type.h
@@ -1,18 +1,20 @@
 #ifndef TYPE_H_INCLUDED
 #define TYPE_H_INCLUDED
 
-typedef struct
-{
-  int numero; 
-  int sub; 
-  int price;
-  int old;
-
-} Customer; 
-
 typedef enum 
 {
-  First = 1, Gold = 2, Diamond = 3, Business = 4
-}Sub;
+  First = 1, Gold = 2, Diamond = 3, Business = 4 //Type d'abonnement
+
+}Sub; //Type d'abonnement souscrit 
+
+typedef struct
+{
+  int numero; //numéro client
+  Sub sub; //l'abonnement (First,etc...), elle renvoie aussi à l'énumération Sub
+  int price; //Prix de l'abonnement 
+  int old; //l'ancienneté 
+
+} Customer; //Structure de l'information du client
+
 
 #endif // TYPE_H_INCLUDED
